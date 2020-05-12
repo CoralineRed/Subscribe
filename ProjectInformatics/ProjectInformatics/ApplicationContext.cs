30296b7fd103e14181523bed3aae54bc98f68d4f
Nesom <rakhimovsamatarrr@gmail.com>
2020-05-12 20:37:51 +0300

    first commit

---
+++ ProjectInformatics/ProjectInformatics/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectInformatics.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectInformatics
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
