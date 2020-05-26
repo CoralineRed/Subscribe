58e2a47776fd876b1f0d68d81cc5b760e686bb7c
Nesom <rakhimovsamatarrr@gmail.com>
2020-05-26 17:14:10 +0300

    roles and fix

--- ProjectInformatics/ProjectInformatics/ApplicationContext.cs
+++ ProjectInformatics/ProjectInformatics/ApplicationContext.cs
@@ -7,6 +7,7 @@ namespace ProjectInformatics
     {
         public DbSet<Message> Messages { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Service> Services { get; set; }
         public DbSet<Subscription> Subscriptions { get; set; }
@@ -15,6 +16,23 @@ namespace ProjectInformatics
             : base(options)
         {
             Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            string adminRoleName = "admin";
+            string userRoleName = "user";
+
+            string adminEmail = "admin123@mail.ru";
+            string adminPassword = "12345678";
+
+            // добавляем роли
+            Role adminRole = new Role { Id = 1, Name = adminRoleName };
+            Role userRole = new Role { Id = 2, Name = userRoleName };
+            User adminUser = new User { Id = 1, Email = adminEmail, Password = adminPassword, RoleId = adminRole.Id };
+
+            modelBuilder.Entity<Role>().HasData(new Role[] { adminRole, userRole });
+            modelBuilder.Entity<User>().HasData(new User[] { adminUser });
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
