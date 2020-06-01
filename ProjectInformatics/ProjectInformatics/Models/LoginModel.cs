2fb396312d9b78eda09b629674f3b2e6b0971409
Askar <42945205+Feinei@users.noreply.github.com>
2020-06-01 16:08:48 +0300

    add login validation

--- ProjectInformatics/ProjectInformatics/Models/LoginModel.cs
+++ ProjectInformatics/ProjectInformatics/Models/LoginModel.cs
@@ -4,9 +4,11 @@ namespace ProjectInformatics.Models
 {
     public class LoginModel
     {
+        [Display(Name ="Введите логин")]
         [Required(ErrorMessage = "Не указан Email")]
         public string Email { get; set; }
 
+        [Display(Name = "Введите пароль")]
         [Required(ErrorMessage = "Не указан пароль")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
